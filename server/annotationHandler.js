6549dcaa04fd7154a4cd4c87174d662e27653734
Aashbir Basi <65787256+bclaashbir@users.noreply.github.com>
2025-02-27 18:07:24 -0800

    Add files via upload

--- server/annotationHandler.js
+++ server/annotationHandler.js
@@ -12,25 +12,33 @@ module.exports = (app) => {
   }
   const db = new sqlite3.Database('./xfdf.db');
   db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS ${TABLE} (documentId TEXT, annotationId TEXT PRIMARY KEY, xfdfString TEXT)`);
+    db.run(`CREATE TABLE IF NOT EXISTS ${TABLE} (documentId TEXT, annotationId TEXT PRIMARY KEY, xfdfString TEXT, widget TEXT)`);
   });
 
   // Connect to WebSocket client
   wss.on('connection', ws => {   
     // When message is received from client
-    ws.on('message', message => {
-      const documentId = JSON.parse(message).documentId;
-      const annotationId = JSON.parse(message).annotationId;
-      const xfdfString = JSON.parse(message).xfdfString.replace(/\'/g, `''`);
+    //ws.on('message', message => {
+    ws.on('message', function message(data, isBinary) {
+      //const documentId = JSON.parse(message).documentId;
+      //const annotationId = JSON.parse(message).annotationId;
+      //const xfdfString = JSON.parse(message).xfdfString.replace(/\'/g, `''`);
+      const documentId = JSON.parse(data).documentId;
+      const annotationId = JSON.parse(data).annotationId;
+      const widget = JSON.parse(data).widget;
+      console.log(data);
+      //const xfdfString = JSON.parse(data).xfdfString.replace(/\'/g, `''`);
+      const xfdfString = JSON.parse(data).xfdfString;
       // Prepare statement to sanitize input
-      let statement = db.prepare(`INSERT OR REPLACE INTO annotations VALUES (?, ?, ?)`);
+      let statement = db.prepare(`INSERT OR REPLACE INTO annotations VALUES (?, ?, ?, ?)`);
       db.serialize(() => {
-        statement.run(documentId, annotationId, xfdfString);
+        statement.run(documentId, annotationId, xfdfString, widget);
       });
       wss.clients.forEach((client) => {
         // Broadcast to every client except for the client where the message came from
         if (client.readyState === WebSocket.OPEN && ws !== client) {
-          client.send(message);
+          //const message = isBinary ? data : data.toString();
+          client.send(data, {binary: isBinary});
         } 
       });
     })
