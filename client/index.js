bf54eaaca5287a360e63a297369ca5341c759cff
Aashbir Basi <65787256+bclaashbir@users.noreply.github.com>
2025-02-27 18:06:32 -0800

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -14,7 +14,8 @@ connection.onerror = error => {
 
 WebViewer.Iframe({
   path: 'lib', // path to the PDFTron 'lib' folder
-  initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
+  initialDoc: 'extractedDocument (2).pdf',
+  l: 'INSERT TRIAL KEY HERE',
   documentXFDFRetriever: async () => {
     const rows = await loadXfdfStrings(DOCUMENT_ID);
     return JSON.parse(rows).map(row => row.xfdfString);
@@ -22,17 +23,53 @@ WebViewer.Iframe({
 }, viewerElement).then( instance => {
 
   // Instance is ready here
-  instance.UI.openElements(['leftPanel']);
+  const tool = instance.Core.documentViewer.getTool('AnnotationCreateSignature');
+  tool.setSigningMode(instance.Core.Tools.SignatureCreateTool.SigningModes.ANNOTATION);
+  instance.UI.enableFeatures([instance.UI.Feature.Initials]);
   annotationManager = instance.Core.documentViewer.getAnnotationManager();
   // Assign a random name to client
   annotationManager.setCurrentUser(nameList[Math.floor(Math.random()*nameList.length)]);
-  annotationManager.addEventListener('annotationChanged', async e => {
+
+
+  instance.Core.documentViewer.addEventListener('annotationsLoaded', () => {
+    const widgets = annotationManager.getAnnotationsList().filter(annotation => annotation instanceof instance.Core.Annotations.WidgetAnnotation);
+    widgets.forEach((widget) => {
+      widget.addEventListener('mousedown', async () => {
+        console.log("widget pressed");
+        annotList = [await widget];
+        const xfdf = await annotationManager.exportAnnotations({annotationList: annotList, widgets: true, fields: false});
+        console.log(xfdf);
+        const widgetXfdf = cleanupXML(xfdf, widget.fieldName);
+        console.log(widgetXfdf);
+        sendWidgetChange(widgetXfdf, widget.Id);
+
+        });
+      });
+    });
+
+
+  annotationManager.addEventListener('fieldChanged', async e => {
+    console.log("field changed");
+
+    const xfdfString = await annotationManager.exportAnnotationCommand();
+    console.log(xfdfString);
+    const parser = new DOMParser();
+    const commandData = parser.parseFromString(xfdfString, 'text/xml');
+
+    sendFieldChange(xfdfString, e.field, e.value);
+  });
+
+
+  annotationManager.addEventListener('annotationChanged', async (annotations, action, {imported}) => {
     // If annotation change is from import, return
-    if (e.imported) {
+    if (imported) {
       return;
     }
 
+    console.log("annotation changed");
+
     const xfdfString = await annotationManager.exportAnnotationCommand();
+    console.log(xfdfString);
     // Parse xfdfString to separate multiple annotation changes to individual annotation change
     const parser = new DOMParser();
     const commandData = parser.parseFromString(xfdfString, 'text/xml');
@@ -57,9 +94,36 @@ WebViewer.Iframe({
   });
 
   connection.onmessage = async (message) => {
+    console.log("message:");
+    console.log(message);
+    console.log(message.data);
+    //console.log(JSON.parse(message).annotationId);
     const annotation = JSON.parse(message.data);
+    console.log(annotation.xfdfString);
+    if (annotation.widget)
+    {
+      console.log("should import annotation");
+      const annotations = await annotationManager.importAnnotations(annotation.xfdfString);
+      await annotationManager.drawAnnotationsFromList(annotations);
+      annotations.forEach((widget) => {
+        widget.addEventListener('mousedown', async () => {
+          console.log("widget pressed");
+          annotList = [await widget];
+          const xfdf = await annotationManager.exportAnnotations({annotationList: annotList, widgets: true, fields: false});
+          console.log(xfdf);
+          const widgetXfdf = cleanupXML(xfdf, widget.fieldName);
+          console.log(widgetXfdf);
+          sendWidgetChange(widgetXfdf, widget.Id);
+          });
+        });
+    }
+    else
+    {
+    console.log("should import annotation command");
     const annotations = await annotationManager.importAnnotationCommand(annotation.xfdfString);
+    console.log(annotations);
     await annotationManager.drawAnnotationsFromList(annotations);
+    }
   }
 });
 
@@ -101,7 +165,93 @@ const sendAnnotationChange = (annotation, action) => {
     connection.send(JSON.stringify({
       documentId: DOCUMENT_ID,
       annotationId: annotation.getAttribute('name'),
-      xfdfString: convertToXfdf(annotationString, action)
+      xfdfString: convertToXfdf(annotationString, action),
+      widget: false
     }));
   }
+}
+
+const sendFieldChange = (xfdf, name, value) => {
+    connection.send(JSON.stringify({
+      documentId: DOCUMENT_ID,
+      annotationId: name,
+      xfdfString: xfdf,
+      widget:false
+    }));
+}
+
+const sendWidgetChange = (xfdf, name) => {
+  connection.send(JSON.stringify({
+    documentId: DOCUMENT_ID,
+    annotationId: name,
+    xfdfString: xfdf,
+    widget: true
+  }));
+}
+
+//tested ai by asking it to write something to parse the xfdf to get the widget and field info we're interested in, did a pretty good job in producing something that worked,
+//though i did do some cleanup to streamline things, should revisit this as it can be cleaned up a bit more
+function cleanupXML(xmlString, fieldName) {
+  // Create a new DOM parser
+  const parser = new DOMParser();
+  
+  // Parse the XML string into a DOM object
+  const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+  
+  // Define the XML namespaces
+  const nsResolver = (prefix) => {
+      const ns = {
+          //'': 'http://ns.adobe.com/xfdf/',
+          'xfdf': 'http://ns.adobe.com/xfdf/',
+          'pdfinfo': 'http://www.pdftron.com/pdfinfo'
+      };
+      return ns[prefix] || null;
+  };
+  
+  // Find the field node based on the provided field name
+  const fieldNode = xmlDoc.evaluate(`//pdfinfo:ffield[@name='${fieldName}']`, xmlDoc, nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  
+  if (!fieldNode) {
+      return `Field '${fieldName}' not found.`;
+  }
+
+  // Serialize the field node to an XML string
+  const fieldXml = new XMLSerializer().serializeToString(fieldNode);
+  
+  // Find the widget node associated with the field
+  const widgetNode = xmlDoc.evaluate(`//pdfinfo:widget[@field='${fieldName}']`, xmlDoc, nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  
+  if (!widgetNode) {
+      return {
+          field: fieldXml,
+          widget: null
+      };
+  }
+
+  const captionsNode = xmlDoc.evaluate(`//pdfinfo:widget[@field='${fieldName}']/pdfinfo:captions`, xmlDoc, nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        
+  if (captionsNode) {
+      // Update the Normal property
+      const normalValue = captionsNode.getAttribute('Normal');
+      if (normalValue === ' ') {
+          captionsNode.setAttribute('Normal', 'O');
+      } else if (normalValue === 'O') {
+          captionsNode.setAttribute('Normal', 'Ø');
+      } else if (normalValue === 'Ø') {
+          captionsNode.setAttribute('Normal', ' ');
+      }
+    }
+
+  const widgetXml = new XMLSerializer().serializeToString(widgetNode);
+  
+  const simplifiedXML = `<?xml version="1.0" encoding="UTF-8" ?>
+  <xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve">
+    <pdf-info xmlns="http://www.pdftron.com/pdfinfo" version="2" import-version="4">
+      ${fieldXml}
+      ${widgetXml}
+    </pdf-info>
+    <annots />
+  </xfdf>`;
+      
+  return simplifiedXML;
 }
